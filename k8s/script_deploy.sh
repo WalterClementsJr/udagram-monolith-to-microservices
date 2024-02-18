a52225016f465c946a8046117152693a90827017
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2024-02-18 13:58:29 +0700

    test deploy script #1

--- k8s/script_deploy.sh
+++ k8s/script_deploy.sh
@@ -6,6 +6,8 @@ cd k8s
 
 if [ -z "$KUBECONFIG" ]
 then
+    echo "kubectl config start"
+
     echo ${KUBERNETES_CA} | base64 --decode > udagram-ca.pem
     echo ${KUBERNETES_CLIENT_CA} | base64 --decode > udagram-client-ca.pem
     echo ${KUBERNETES_CLIENT_KEY} | base64 --decode > udagram-key.pem
@@ -13,7 +15,11 @@ then
     kubectl config set-credentials kubernetes-admin --client-certificate=udagram-client-ca.pem --client-key=udagram-key.pem
     kubectl config set-context kubernetes-admin@udagram --cluster=udagram --namespace=default --user=kubernetes-admin
     kubectl config use-context kubernetes-admin@udagram
+
+    echo "kubectl config completed"
 fi
+
+echo "kubectl apply steps start"
 
 secret=$(envsubst '$JWT_SECRET $POSTGRES_USERNAME $POSTGRES_PASSWORD' < env-secret.yml)
 echo "secret: $secret"# | kubectl apply -f -
@@ -23,6 +29,8 @@ echo "configmap: $configmap"# | kubectl apply -f -
 
 awsSecret=$(envsubst '$AWS_CREDENTIALS' < aws-secret.yml)
 echo "awsSecret: $awsSecret"# | kubectl apply -f -
+
+echo "kubectl apply steps ok"
 
 #kubectl apply -f deployment-api-feed.yml
 #kubectl apply -f deployment-api-user.yml
